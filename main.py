10f33f5bdf08ad6261e795a06a071f492f4efe31
justjo3l <comedyandcomputers@gmail.com>
2022-05-01 15:48:31 +1000

    Removed Database and added requirements.txt

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
-from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
 
 from scrapeFiles import scrapeMatch
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
 class Match(Resource):
     def get(self, match_id):
         result = scrapeMatch.find_matches(match_id)
